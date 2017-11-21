85103c1d481d3f3d8ade869549490dfe9b3d61b0
Antoine Barbez <antoine.brbz@gmail.com>
2017-11-21 15:35:57 -0500

    add constructDataset method + saved the co-occurence matrices + remove some systems history files

--- historical_anti-pattern_detection/reader.py
+++ historical_anti-pattern_detection/reader.py
@@ -5,25 +5,16 @@ import csv
 import os
 import sys
 import fnmatch
+import pickle
 
 import numpy as np
 
-
-def read(csvFile):
-	with open(csvFile, 'rb') as csvfile:
-		reader = csv.DictReader(csvfile, delimiter=';')
-		changes = []
-		for row in reader:
-			if row['Entity'] == 'METHOD':
-				change = {}
-				change['Snapshot'] = row['Snapshot']
-				change['Methode'] = row['Code']
-				changes.append(change)
-
-		return changes
+''' This file contains all the methods that we will use to extract the data from 
+	the differents csv files (historical information, anti-pattern occurences...)'''
 
 def readHistory(csvFile):
 	with open(csvFile, 'rb') as csvfile:
+		#print('start readind file :', csvFile)
 		reader = csv.DictReader(csvfile, delimiter=';')
 		changes = []
 		for row in reader:
@@ -48,6 +39,41 @@ def readHistory(csvFile):
 
 #return the co-occurence matrix of the different classes
 def getCoocMatrix(changes):
+	print('start getting cooccurence matrix...')
+	data = []
+	classOcc = []
+	commit = []
+	commitNumber = changes[0]['Snapshot']
+	for i, change in enumerate(changes):
+		classOcc.append(change['Class'])
+		if commitNumber != change['Snapshot']:
+			data.append(commit)
+			commit = []
+			commitNumber = change['Snapshot']
+
+		commit.append(change['Class'])
+		if i == len(changes)-1:
+			data.append(commit)
+
+	classes = list(set(classOcc))
+	size = len(classes)
+	reverseDictionnary = {classes[i]: i for i in xrange(size)}
+
+	coocMatrix = np.zeros((size,size), dtype=np.int16)
+
+	#TODO this last part is very long at execution, try to reduce it !
+	for commit in data:
+		oneHotCommit = np.zeros((size,1), dtype=np.int16)
+		for className in set(commit):
+			oneHotCommit[reverseDictionnary[className],0] = 1;
+
+		coocMatrix += oneHotCommit.dot(oneHotCommit.T)
+	return reverseDictionnary , coocMatrix
+
+
+#only use for test purpose
+def getReverseDictionnary(csvFile):
+	changes = readHistory(csvFile)
 
 	data = []
 	classOcc = []
@@ -68,23 +94,27 @@ def getCoocMatrix(changes):
 	size = len(classes)
 	reverseDictionnary = {classes[i]: i for i in xrange(size)}
 
-	coocMatrix = np.zeros((size,size), dtype=np.int32)
-	
-	for commit in data:
-		oneHotCommit = np.zeros(size, dtype=np.int32)
-		for className in set(commit):
-			oneHotCommit[reverseDictionnary[className]] = 1;
+	return reverseDictionnary
 
-		c = oneHotCommit.reshape(size,1)
-		coocMatrix += c.dot(c.T)
 
-	return coocMatrix
+def saveCoocMatrices():
+	for path,dirs,files in os.walk('./data/systems_history'):
+
+		for f in fnmatch.filter(files,'*.csv'):
+			historyFile = './data/systems_history/' + f
+			fName = f.split('.')[0]
+			coocFile = './data/co-occurence_matrices/' + fName + '.pickle'
+
+			changes = readHistory(historyFile)
+			reverseDictionnary , coocMatrix = getCoocMatrix(changes)
+			data = (reverseDictionnary , coocMatrix)
+
+			with open(coocFile, 'wb') as file:
+				pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 #return the conditional probability matrix of the different classes
-def getCPM(csvFile):
-	changes = readHistory(csvFile)
-	coocMatrix = getCoocMatrix(changes)
-
+def getCPM(coocMatrix):
 	'''concidering ci=True if the class i change in a commit ,
 	CPM(i,j) = P(cj|ci) = coocMatrix(i,j)/coocMatrix(i,i)'''
 	size = len(coocMatrix)
@@ -97,6 +127,66 @@ def getCPM(csvFile):
 
 	return CPM
 
+''' return instances and labels that will be used to train some models later.
+	The instances represents classes of a software 
+	and are of the form of a (nbClasses - 1 , 2) matrix where 
+	nbClasses is the number of classes in the given system.
+
+	concidering I the instance corresponding to the ith class (ci) of a system,
+	I(j,1) = P(ci | cj) and I(j, 2) = P(cj | ci)
+	
+'''
+def constructDataset():
+	instances = []
+	labels = np.array([])
+
+	for path,dirs,files in os.walk('./data/co-occurence_matrices'):
+
+		for f in fnmatch.filter(files,'*.pickle'):
+			fileName = f.split('.')[0]
+			coocFile = './data/co-occurence_matrices/' + f
+			blobOccurencesFile = './data/blob/' + fileName + '.csv'
+
+			with open(coocFile, 'r') as open_file:
+				reverseDictionnary , coocMatrix = pickle.load(open_file)
+				CPM = getCPM(coocMatrix)
+
+			blobOccurences = np.zeros(len(reverseDictionnary))
+
+			with open(blobOccurencesFile, 'rb') as csvfile:
+				reader = csv.reader(csvfile, delimiter=';')
+
+				blobOccurences = np.zeros(len(reverseDictionnary))
+				for row in reader:
+					blobOccurences[reverseDictionnary[row[0]]] = 1
+
+				labels = np.append(labels, blobOccurences)
+
+			for i in range(len(reverseDictionnary)):
+				c1 = CPM[:,i]
+				c2 = CPM[i,:]
+				c1 = np.delete(c1,i)
+				c2 = np.delete(c2,i)
+
+				c = np.append(c1,c2).reshape(2,len(c1)).T
+				instances.append(c)
+
+	return instances , labels
+
+
+#used to test an idea, not important ....
+def read(csvFile):
+	with open(csvFile, 'rb') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=';')
+		changes = []
+		for row in reader:
+			if row['Entity'] == 'METHOD':
+				change = {}
+				change['Snapshot'] = row['Snapshot']
+				change['Methode'] = row['Code']
+				changes.append(change)
+
+		return changes
 
 def data2Text(csvFile):
 	changes = read(csvFile)
@@ -117,25 +207,6 @@ def data2Text(csvFile):
 
 
 if __name__ == "__main__":
-	'''for path,dirs,files in os.walk('./systems_history'):
-		for f in fnmatch.filter(files,'*.csv'):
-			fullname = os.path.abspath(os.path.join(path,f))
-			changes = read(fullname)
-
-			snapshots = []
-			methods = []
-
-			for change in changes:
-				snapshots.append(change['Snapshot'])
-				methods.append(change['Methode'])
-
-			ratio = (len(methods)/len(set(methods)))
-			print('system name :', f)
-			print('nb snapshot :',len(set(snapshots)))
-			print('methods :', ratio)'''
-
-	#data2Text('./systems_history/frameworks-base.csv')
-
 	'''changes = [
 		{'Snapshot': '1', 'Class':'a'},
 		{'Snapshot': '1', 'Class':'b'},
@@ -144,14 +215,35 @@ if __name__ == "__main__":
 		{'Snapshot': '3', 'Class':'a'},
 		{'Snapshot': '3', 'Class':'c'},
 		{'Snapshot': '3', 'Class':'b'},
-		{'Snapshot': '4', 'Class':'b'},
+		{'Snapshot': '4', 'Class':'d'},
 		{'Snapshot': '4', 'Class':'b'},
 		{'Snapshot': '4', 'Class':'c'},
+		{'Snapshot': '5', 'Class':'d'},
+		{'Snapshot': '6', 'Class':'a'},
+		{'Snapshot': '6', 'Class':'a'},
+		{'Snapshot': '6', 'Class':'b'},
+		{'Snapshot': '7', 'Class':'b'},
+		{'Snapshot': '7', 'Class':'c'},
 	]
 
-	coocMatrix = getCoocMatrix(changes)'''
-	CPM = getCPM('./data/systems_history/frameworks-support.csv')
-	print(CPM)
+	reverseDictionnary , coocMatrix = getCoocMatrix(changes)
+	print(reverseDictionnary)
+	print(coocMatrix)
+	data = (reverseDictionnary , coocMatrix)
+
+	with open('./data/co-occurence_matrix/test.pickle', 'wb') as file:
+		pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+	with open('./data/co-occurence_matrix/test.pickle', 'r') as open_file:
+            rd , cM = pickle.load(open_file)
+            print(rd)
+            print(cM)'''
+
+	#x , y = constructDataset()
+	#print(x[1733].shape)
+	#print(y.shape)
+	#print(y.sum())
+
 
 	
 
